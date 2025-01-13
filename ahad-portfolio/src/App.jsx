f08ee0ca84d7b8d7b579c92615ed69b7f26ace1b
Ahad-Awan <muhammadahad203813@gmail.com>
2025-01-13 18:24:50 +0500

    about me text complete

--- ahad-portfolio/src/App.jsx
+++ ahad-portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import NavBarMain from "./components/navbar/NavBarMain";
 import HeroMain from "./components/heroSection/heroMain";
 import HeroGradient from "./components/heroSection/heroGradient";
 import SubHeroSection from "./components/heroSection/SubHeroSection";
+import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <HeroMain />
       <HeroGradient />
       <SubHeroSection />
+      <AboutMeMain />
     </main>
   );
 }
---
+++ ahad-portfolio/src/components/aboutMeSection/AboutMeText.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AboutMeText = () => {
+  return (
+    <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center ">
+      <h2 className="text-6xl text-cyan mb-8">About Me</h2>
+      <p className="text-white">
+        I am an adaptable, conscientious, and ambitious individual, able to
+        quickly acquire new skills. I thrive in high tech environments where my
+        educational background and professional skills can be effectively
+        utilized. I seek opportunities that offer challenges and enable me to
+        contribute to cutting edge advancements while continuously growing in my
+        career.
+      </p>
+      <button className="border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-black">
+        My Projects
+      </button>
+    </div>
+  );
+};
+
+export default AboutMeText;
