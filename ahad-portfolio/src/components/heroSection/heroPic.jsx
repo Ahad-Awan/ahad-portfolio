71d6d330ebac81f889e7ec11db16437c311cf497
Ahad-Awan <muhammadahad203813@gmail.com>
2025-01-13 13:13:53 +0500

    change pic

--- ahad-portfolio/src/components/heroSection/heroPic.jsx
+++ ahad-portfolio/src/components/heroSection/heroPic.jsx
@@ -5,13 +5,10 @@ const heroPic = () => {
   return (
     <div className="h-full flex items-center justify-center">
       <img
-        src="../../public/images/hexaPic.png"
+        src="../../public/images/ahadPic.png"
         alt="ahad image"
-        className="max-h-[550px] w-auto"
+        className="max-h-[600px] w-auto"
       />
-      <div className="absolute -z-10 justify-center items-center animate-pulse">
-        <PiHexagonThin className="md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
-      </div>
     </div>
   );
 };
