e476448af742368b23867bae6f0f44e6a9a9fee1
Ahad-Awan <muhammadahad203813@gmail.com>
2025-01-13 19:11:45 +0500

    About me section complete

--- ahad-portfolio/src/components/aboutMeSection/AboutMeText.jsx
+++ ahad-portfolio/src/components/aboutMeSection/AboutMeText.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const AboutMeText = () => {
   return (
     <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center ">
-      <h2 className="text-6xl text-cyan mb-8">About Me</h2>
-      <p className="text-white">
+      <h2 className="text-6xl text-cyan mb-10">About Me</h2>
+      <p className="text-white text-[17.5px]">
         I am an adaptable, conscientious, and ambitious individual, able to
         quickly acquire new skills. I thrive in high tech environments where my
         educational background and professional skills can be effectively
