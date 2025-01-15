e0cc05568432f3260ceff937f52b0e80783ffafd
Ahad-Awan <muhammadahad203813@gmail.com>
2025-01-15 22:05:46 +0500

    hero section motion add

--- ahad-portfolio/src/components/heroSection/heroPic.jsx
+++ ahad-portfolio/src/components/heroSection/heroPic.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 const heroPic = () => {
   return (
-    <div className="h-full flex items-center justify-center">
+    <motion.div
+      variants={fadeIn("left", 0.4)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0 }}
+      className="h-full flex items-center justify-center"
+    >
       <img
         src="/images/ahadPic.png"
         alt="ahad image"
         className="max-h-[500px] w-auto"
       />
-    </div>
+    </motion.div>
   );
 };
 
