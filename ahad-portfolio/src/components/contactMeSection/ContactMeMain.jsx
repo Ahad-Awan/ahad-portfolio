e109652c9c70c5ee9de6fb206e4de9657ba6ad91
Ahad-Awan <muhammadahad203813@gmail.com>
2025-01-15 18:06:00 +0500

    add contact me section

---
+++ ahad-portfolio/src/components/contactMeSection/ContactMeMain.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ContactMeLeft from "./ContactMeLeft";
+import ContactMeRight from "./ContactMeRight";
+
+const ContactMeMain = () => {
+  return (
+    <div>
+      <h2></h2>
+      <div>
+        <ContactMeLeft />
+        <ContactMeRight />
+      </div>
+    </div>
+  );
+};
+
+export default ContactMeMain;
